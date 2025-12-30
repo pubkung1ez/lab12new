330612355c3fff1390503f3ab94faf64d919fdc8
pubkung1ez <tirapan_30506@sysp.ac.th>
2025-12-30 15:29:44 +0700

    suss

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int a);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int a){
+    if (a == 1) return 1;
+    if (a == 0) return 0;
+    return fibonacci(a-1) + fibonacci(a-2);
 }
